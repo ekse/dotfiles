9d160a9afa7011c5534a2130f6791c0b9e101bed
Sébastien Duquette <ekse.0x@gmail.com>
2015-04-12 14:33:26 -0400

    renamed fish.config to config.fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,12 @@
+alias s=sudo
+alias l=ls
+alias ll="ls -l"
+alias py=python
+alias ipy=ipython
+alias yavide="gvim --servername yavide -N -u /opt/yavide/.vimrc"
+
+set fish_greeting ""
+set EDITOR vim
+set TERM xterm-256color
+set CC clang
+set CXX clang++
